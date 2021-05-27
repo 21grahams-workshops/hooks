34fdf9968d2301b7ffe984a9ee3fc11c440c7f1c
21grahams <graham.kirsh@gmail.com>
2021-05-27 11:41:23 -0600

    added CounterClass component using class components

--- hooks_intro/src/App.js
+++ hooks_intro/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import CounterClass from './CounterClass';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-
+        <CounterClass />
       </div>
     )
   }
