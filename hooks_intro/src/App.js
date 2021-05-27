41c20e103cb60003f1091cb05079d864e7af2c4f
21grahams <graham.kirsh@gmail.com>
2021-05-27 12:20:10 -0600

    implement custom hook useToggler, created in separte file and reused in Toggler.js

--- hooks_intro/src/App.js
+++ hooks_intro/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CounterClass from './CounterClass';
 import CounterHooks from './CounterHooks';
+import Toggler from './Toggler';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className='App'>
         <CounterClass />
         <CounterHooks />
+        <Toggler />
       </div>
     )
   }
