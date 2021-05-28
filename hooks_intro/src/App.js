7d990d551a3fe72ab36c5219d3eb8013f18c32a5
21grahams <graham.kirsh@gmail.com>
2021-05-27 23:20:52 -0600

    finalized useEffect to call a star wars API

--- hooks_intro/src/App.js
+++ hooks_intro/src/App.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
-import CounterClass from './CounterClass';
-import CounterHooks from './CounterHooks';
-import Toggler from './Toggler';
-import SimpleFormHooks from './SimpleFormHooks';
-import SimpleFormInputHook from './SimpleFormInputHook';
-import Clicker from './Clicker';
+// import CounterClass from './CounterClass';
+// import CounterHooks from './CounterHooks';
+// import Toggler from './Toggler';
+// import SimpleFormHooks from './SimpleFormHooks';
+// import SimpleFormInputHook from './SimpleFormInputHook';
+// import Clicker from './Clicker';
+import SWMovies from './SWMovies'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <CounterClass />
+        {/* <CounterClass />
         <CounterHooks />
         <Toggler />
         <SimpleFormHooks />
         <SimpleFormInputHook />
-        <Clicker />
+        <Clicker /> */}
+        <SWMovies />
       </div>
     )
   }
---
+++ hooks_intro/src/SWMovies.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function SWMovies() {
+
+  const [number, setNumber] = useState(1);
+  const [movie, setMovie] = useState('');
+
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get(`https://swapi.dev/api/people/${number}/`);
+      console.log('response: ', response.data);
+      setMovie(response.data)
+    }
+    getData();
+  }, [number])
+
+  return (
+    <div>
+      <h1>Pick A Movie</h1>
+      <h4>{movie.name}</h4>
+      <p>{movie.birth_year}</p>
+      <select value={number} onChange={e => setNumber(e.target.value)}>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option>
+        <option value='6'>6</option>
+        <option value='7'>7</option>
+      </select>
+    </div>
+  )
+}
+
+export default SWMovies;
